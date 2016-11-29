0e27ad192b0d4d46876b0197996041f28e8be39e
MiguelMedeiros\Miguel Medeiros <miguel.medeiros@gmail.com>
2016-11-29 01:27:46 -0200

    Initial commit. Version 1.0 Yoda loves Wordpress Theme.

---
+++ footer.php
@@ -0,0 +1,16 @@
+            <!-- FOOTER -->
+		<footer>
+                  
+		</footer>
+            <!-- FOOTER -->
+
+            <!-- JS -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <script src="<?php echo get_template_directory_uri();?>/assets/js/scripts.min.js"></script>
+            <!-- JS -->
+
+            <!-- WP FOOTER -->
+            <?php wp_footer();?>
+            <!-- WP FOOTER -->
+      </body>
+</html>
